9633577d38200ddb9c91e88570b08c8fd984c9c0
Nadeen Noshy <nadeennoshy3@gmail.com>
2024-02-10 12:15:18 +0200

    language learning app

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:language_learning_app/views/home_view.dart';
+
+void main(){
+  runApp(const LanguageLearningApp());
+}
+class LanguageLearningApp extends StatelessWidget {
+  const LanguageLearningApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const MaterialApp(
+      home:HomeView(),
+    );
+  }
+}
